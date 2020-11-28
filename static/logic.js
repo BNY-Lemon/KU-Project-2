fe3033771cd08b9ff198fa58ad0d40880d570d98
BNY-Lemon <BNY-Lemon@users.noreply.github.com>
2020-11-28 11:04:59 -0600

    Fixed two tables in db. Pins work in Leaflet.

--- static/logic.js
+++ static/logic.js
@@ -4,24 +4,21 @@ var url = "/api/v1.0/happiness";
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    objArray=[];
-   
-   objArray = transformData(data,"Latitude")
-    console.log(objArray)
- //   console.log(data)
+    console.log(data)
   });
   
-function transformData(data, key) {
-    objArray=[];
+// function transformData(data, key) {
+//     objArray=[];
    
-    Object.entries(data[0][key]).forEach(e=> {
-      obj = {};
-     obj[e[0]] = e[1]
-     objArray.push(obj)
+//    + Object.entries(data[0][key]).forEach(e=> {
+//       obj = {};
+//      obj[e[0]] = e[1]
+//      objArray.push(obj)
      
-    })
-  return objArray
-}
+//     })
+//   return objArray
+// }
+
 // Create a map object
 var myMap = L.map("map", {
   center: [37.09, -95.71],
@@ -45,10 +42,9 @@ d3.json(url, function(data) {
   for (var i = 0; i < data.length; i++) {
     try {
       // nonExistentFunction();
-        var country = data[0][i];
-        L.marker([country.Latitude, country.Longitude])
-          .bindPopup("<h1> Test </h1>")
-          .openPopup()
+        var country = data[i];
+        L.marker([country.Latitude, country.Longitude]) //
+          .bindPopup("<h1>" + country.Country + "</h1> <hr> <h3>Happiness Ladder Score " + country["Ladder score"] + "</h3>") // finish adding rest of data
           .addTo(myMap);
         console.log("random");
     } catch {
@@ -58,67 +54,3 @@ d3.json(url, function(data) {
     }
   }
 })
-// // // "<h1>" + country.id + "</h1> <hr> <h3>Happiness Ladder Score " + country["Ladder score"] + "</h3>"
-
-
-// Try something new:
-// Creating map object
-
-// function swap(json){
-//   var ret = {};
-//   for(var key in json){
-//     ret[json[key]] = key;
-//   }
-//   return ret;
-// }
-
-// var myMap = L.map("map", {
-//   center: [40.7, -73.95],
-//   zoom: 11
-// });
-
-// // Adding tile layer to the map
-// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "mapbox/streets-v11",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
-// // Store API query variables
-// var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
-// var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
-// var complaint = "&complaint_type=Rodent";
-// var limit = "&$limit=10000";
-
-// // Assemble API query URL
-// var url = baseURL + date + complaint + limit;
-
-// // Grab the data with d3
-// d3.json(url, function(response) {
-
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
-
-//   // Loop through data
-//   for (var i = 0; i < response.length; i++) {
-
-//     // Set the data location property to a variable
-//     var location = response[i].location;
-
-//     // Check for location property
-//     if (location) {
-
-//       // Add a new marker to the cluster group and bind a pop-up
-//       markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-//         .bindPopup(response[i].descriptor));
-//     }
-
-//   }
-
-//   // Add our marker cluster layer to the map
-//   myMap.addLayer(markers);
-
-// });
